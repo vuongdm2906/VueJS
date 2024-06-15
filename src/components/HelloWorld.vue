d728dd29e0bc80b262207a617425dc3702c72587
vuongdo296 <150891568+vuongdm2906@users.noreply.github.com>
2024-06-15 11:22:29 +0700

    day1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,21 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+  </div>
+  <div>
+    <input type="text" v-model="value">
+    <button @click="handleTodoList">ADD</button>
+  </div>
+  <div>
+    <ul v-for="(value, index) in todoList" :key="index">
+      <li><span :class="{ done: value.status }" @click="handleStatus(value)">{{ value.content }}</span><button
+          @click="remove(index)">Xoa</button></li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+  </div>
+  <div>
+    <h5 v-show="completedTask.length >= 1">Danh sach cong viec hoan thanh</h5>
+    <ul v-for="(value, index) in completedTask" :key="index">
+      <li><span :class="{ done: value.status }" @click="handleStatus(value)">{{ value.content }}</span></li>
     </ul>
   </div>
 </template>
@@ -34,24 +25,70 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      value: '',
+      todoList: [],
+    }
+  },
+  computed: {
+    completedTask() {
+      return this.todoList.filter(value => value.status)
+    }
+  },
+  watch: {
+    todoList: {
+      handler(value) {
+        localStorage.setItem('todoListStorage', JSON.stringify(value))
+      }
+    }
+  },
+  mounted() {
+    const todoList = localStorage.getItem('todoList')
+    if(todoList)
+    {
+      this.todoList = JSON.parse(todoList)
+    }
+  },
+  methods: {
+    handleTodoList() {
+      if (this.value.trim() !== '') {
+        this.todoList.push({ content: this.value, status: false })
+        this.value = ''
+      }
+    },
+    handleStatus(value) {
+      value.status = !value.status
+    },
+    remove(index) {
+      this.todoList.splice(index, 1)
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
 h3 {
-  margin: 40px 0 0;
+  margin : 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
